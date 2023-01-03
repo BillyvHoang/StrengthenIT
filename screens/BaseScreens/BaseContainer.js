93691b37861d28ed98fe850740c2ba064c3cdf51
BillyvHoang <Billyhoang100@gmail.com>
2023-01-02 17:07:34 -0800

    TabBar Size and Images

--- screens/BaseScreens/BaseContainer.js
+++ screens/BaseScreens/BaseContainer.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 //Import Screens
 import HomeScreen from "./HomeScreen";
 import WorkOutScreen from "./WorkOutScreen";
@@ -25,17 +27,38 @@ function OverallContainer() {
       <BottomTabs.Screen
         name="Home"
         component={HomeScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          title: "Home",
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
       />
       <BottomTabs.Screen
         name="WorkOuts"
         component={WorkOutScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          title: "WorkOuts",
+          tabBarLabel: "WorkOuts",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="dumbbell" size={size} color={color} />
+          ),
+        }}
       />
       <BottomTabs.Screen
         name="Account"
         component={Setting}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          title: "Account",
+          tabBarLabel: "Account",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" size={size} color={color} />
+          ),
+        }}
       />
     </BottomTabs.Navigator>
   );
